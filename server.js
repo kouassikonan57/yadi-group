67952384f3ced6c7651cf36fb8736fcc4fca20ea
Kouassi Fernand <kouassifernand650@gmail.com>
2025-07-25 10:18:38 +0000

    Test

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 // Servir les fichiers statiques
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
-// Toutes les routes renvoient vers index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Toutes les routes renvoient vers index.php
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.php"));
 });
 
 const PORT = process.env.PORT || 3000;
